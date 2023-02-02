9dbac8d99d5cbc14cd1c246c1226d05b0cd65504
zhouyu <zyxh01619795@alibaba-inc.com>
2023-02-02 18:38:34 +0800

    ""

--- src/main/java/com/zhouyu/gradleDemo/designModel/Adapter/Adapter.java
+++ src/main/java/com/zhouyu/gradleDemo/designModel/Adapter/Adapter.java
@@ -1,4 +1,65 @@
 package com.zhouyu.gradleDemo.designModel.Adapter;
 
 public class Adapter {
+    public static void main(String[] args) {
+        RoundHole roundHole = new RoundHole(5);
+        RoundPeg roundPeg = new RoundPeg(4);
+        SquarePeg squarePeg = new SquarePeg(10);
+        SquarePegAdapter squarePegAdapter = new SquarePegAdapter(squarePeg);
+
+        System.out.println(roundHole.fits(roundPeg));
+        System.out.println(roundHole.fits(squarePegAdapter));
+    }
+}
+class SquarePegAdapter extends RoundPeg{
+    private SquarePeg peg;
+    public SquarePegAdapter(SquarePeg peg) {
+        this.peg = peg;
+    }
+
+    @Override
+    public double getRadius() {
+        double width = peg.getWidth();
+        double result = Math.sqrt(Math.pow(width / 2, 2) * 2);
+        return result;
+    }
+}
+ class RoundPeg {
+    private double radius;
+    public RoundPeg(){};
+    public RoundPeg(double radius){
+        this.radius = radius;
+    }
+    public double getRadius() {
+        return radius;
+    }
+}
+class SquarePeg {
+    private double width;
+
+    public SquarePeg(double width) {
+        this.width = width;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getSquare() {
+        double result;
+        result = Math.pow(this.width, 2);
+        return result;
+    }
+}
+class RoundHole{
+    private double radius;
+    public RoundHole(double radius){
+        this.radius = radius;
+    }
+    public double getRadius(){
+        return radius;
+    }
+    public boolean fits(RoundPeg peg){
+        return (this.getRadius()>=peg.getRadius());
+    }
 }
--- src/main/java/com/zhouyu/gradleDemo/test.java
+++ src/main/java/com/zhouyu/gradleDemo/test.java
@@ -1,7 +1,33 @@
 package com.zhouyu.gradleDemo;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
 public class test {
-    public static void main(String[] args) {
-        System.out.println(Math.pow(2, 3));
+    static volatile int n = 0;
+//    static AtomicInteger n = new AtomicInteger(0);
+//    static int n = 0;
+    public static void main(String[] args) throws BrokenBarrierException, InterruptedException {
+        Thread t1 = new Thread(() -> {
+
+            System.out.println("t1 start");
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            LockSupport.park();
+
+            System.out.println("t1 stop");
+        });
+        t1.start();
+        LockSupport.unpark(t1);
+
+
+        CountDownLatch countDownLatch = new CountDownLatch(2);
+
+
     }
 }
